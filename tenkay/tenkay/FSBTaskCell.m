4c18d937de83637c5ca7cbc916b94066d0c1126e
Jabari Bell <jabari.bell@pxlflu.net>
2013-01-18 12:42:50 -0500

    happy with the staggering in the animation from add time to edit task icons

--- tenkay/tenkay/FSBTaskCell.m
+++ tenkay/tenkay/FSBTaskCell.m
@@ -11,8 +11,14 @@
 
 @implementation FSBTaskCell {
     UIImage *addTimeIcon;
+    UIImage *editTaskIcon;
+    
     UIButton *addTimeButton;
+    UIButton *editTaskButton;
+    
     UIImageView *addTimeButtonImageView;
+    UIImageView *editTaskButtonImageView;
+    
     CABasicAnimation *animation;
     CABasicAnimation *translationAnimation;
 }
@@ -43,6 +49,14 @@
     [addTimeButtonImageView addSubview:addTimeButton];
     [self.viewForBaselineLayout addSubview:addTimeButtonImageView];
     
+    editTaskIcon = [UIImage imageNamed:@"editTimeIcon"];
+    editTaskButtonImageView = [[UIImageView alloc] initWithFrame:CGRectMake(90, 55, 55, 52)];
+    editTaskButton = [[UIButton alloc] init];
+    editTaskButton.frame = CGRectMake(0, 0, 55, 52);
+    [editTaskButton setImage:editTaskIcon forState:UIControlStateNormal];
+    [editTaskButtonImageView addSubview:editTaskButton];
+    [self.viewForBaselineLayout addSubview:editTaskButtonImageView];
+    
     animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
     animation.duration = 0.4;
     //this is a hack, it should ideally listen for when the opening cell animation is done to fire off
@@ -52,29 +66,42 @@
     animation.delegate = self;
     addTimeButtonImageView.layer.opacity = 0.0;
     [addTimeButtonImageView.layer addAnimation:animation forKey:@"animateOpacity"];
+
+    animation.beginTime = CACurrentMediaTime() + 0.25f;
+    editTaskButtonImageView.layer.opacity = 0.0;
+    [editTaskButtonImageView.layer addAnimation:animation forKey:@"animateOpacity"];
     
     translationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.translation.y"];
-    translationAnimation.duration = 0.05;
+    translationAnimation.duration = 0.10;
     translationAnimation.beginTime = CACurrentMediaTime() + 0.15f;
     translationAnimation.autoreverses = NO;
     translationAnimation.removedOnCompletion = NO;
     translationAnimation.fillMode = kCAFillModeForwards;
-    translationAnimation.fromValue = @5.0;
+    translationAnimation.fromValue = @15.0;
     translationAnimation.toValue = @0.0;
+    
     [addTimeButtonImageView.layer addAnimation:translationAnimation forKey:@"animateLayer"];
+    translationAnimation.beginTime = CACurrentMediaTime() + 0.25f;
+    [editTaskButtonImageView.layer addAnimation:translationAnimation forKey:@"animateLayer"];
 }
 
 -(void) animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
 {
     NSLog(@"dan");
     addTimeButtonImageView.layer.opacity = 1.0;
+    editTaskButtonImageView.layer.opacity = 1.0;
 }
 
 -(void)hideNav
 {
-    [addTimeButton removeFromSuperview];
+    [addTimeButtonImageView removeFromSuperview];
+    [editTaskButtonImageView removeFromSuperview];
+    addTimeButtonImageView = nil;
     addTimeButton = nil;
     addTimeIcon = nil;
+    editTaskButtonImageView = nil;
+    editTaskButton = nil;
+    editTaskIcon = nil;
     animation = nil;
 }
 
