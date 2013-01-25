1fd7cb7fd2974b5c135a7e0de29a3f3c7dc091cf
Jabari Bell <jabari.bell@pxlflu.net>
2013-01-25 14:56:49 -0500

    issue #35 fixed, added stop button... just need to make it functional

--- tenkay/tenkay/FSBTaskCell.m
+++ tenkay/tenkay/FSBTaskCell.m
@@ -85,6 +85,7 @@
     animation.toValue = @1.0;
     animation.delegate = self;
     [self.selectedBackground.layer addAnimation:animation forKey:@"animateOpacity"];
+    [self.playButton setImage:[UIImage imageNamed:@"stopxButtonWhite"] forState:UIControlStateNormal];
 }
 
 - (void)showIsRecordingView
